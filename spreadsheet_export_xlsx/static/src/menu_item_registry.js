f658ea4bfe207b538f25746db78ed481de9298dc
He Jian <hej@eabax.com>
2024-08-06 14:00:48 +0800

    fix

--- spreadsheet_export_xlsx/static/src/menu_item_registry.js
+++ spreadsheet_export_xlsx/static/src/menu_item_registry.js
@@ -9,17 +9,5 @@ const { topbarMenuRegistry } = spreadsheet.registries;
 topbarMenuRegistry.addChild("download", ["data"], {
     name: "下载 XLSX",
     sequence: 0,
-    // action: (env) => {
-    //     env.services.action.doAction({
-    //         type: "ir.actions.client",
-    //         tag: "action_download_spreadsheet",
-    //         params: {
-    //             orm: this.orm,
-    //             name: this.props.name,
-    //             data: this.model.exportData(),
-    //             stateUpdateMessages: [],
-    //         },
-    //     })
-    // },
     action: (env) => env.download(),
 });
