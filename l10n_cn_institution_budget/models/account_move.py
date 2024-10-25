6ef1630f8caf04a1695f357e81ba7081def2fede
He Jian <hej@eabax.com>
2024-10-25 14:28:45 +0800

    fix

--- l10n_cn_institution_budget/models/account_move.py
+++ l10n_cn_institution_budget/models/account_move.py
@@ -26,3 +26,43 @@ class AccountMove(models.Model):
         domain=[('is_budget', '=', True)],
         context={'default_is_budget': True}
     )
+
+    def _get_unbalanced_moves(self, container):
+        moves = container['records'].filtered(lambda move: move.line_ids)
+        if not moves:
+            return
+
+        # /!\ As this method is called in create / write, we can't make the assumption the computed stored fields
+        # are already done. Then, this query MUST NOT depend on computed stored fields.
+        # It happens as the ORM calls create() with the 'no_recompute' statement.
+        self.env['account.move.line'].flush_model(['debit', 'credit', 'balance', 'currency_id', 'move_id'])
+        self._cr.execute('''
+            SELECT line.move_id,
+                   ROUND(SUM(line.debit), currency.decimal_places) debit,
+                   ROUND(SUM(line.credit), currency.decimal_places) credit
+              FROM account_move_line line
+              JOIN account_move move ON move.id = line.move_id
+              JOIN res_company company ON company.id = move.company_id
+              JOIN res_currency currency ON currency.id = company.currency_id
+             WHERE line.move_id IN %s AND line.is_budget = False
+          GROUP BY line.move_id, currency.decimal_places
+            HAVING ROUND(SUM(line.balance), currency.decimal_places) != 0
+        ''', [tuple(moves.ids)])
+        result = self._cr.fetchall()
+        if result:
+            return result
+
+        self.env['account.move.line'].flush_model(['debit', 'credit', 'balance', 'currency_id', 'move_id'])
+        self._cr.execute('''
+            SELECT line.move_id,
+                   ROUND(SUM(line.debit), currency.decimal_places) debit,
+                   ROUND(SUM(line.credit), currency.decimal_places) credit
+              FROM account_move_line line
+              JOIN account_move move ON move.id = line.move_id
+              JOIN res_company company ON company.id = move.company_id
+              JOIN res_currency currency ON currency.id = company.currency_id
+             WHERE line.move_id IN %s AND line.is_budget = True
+          GROUP BY line.move_id, currency.decimal_places
+            HAVING ROUND(SUM(line.balance), currency.decimal_places) != 0
+        ''', [tuple(moves.ids)])
+        return self._cr.fetchall()
--- l10n_cn_institution_budget/models/account_move_line.py
+++ l10n_cn_institution_budget/models/account_move_line.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import api, fields, models, Command, _
+from odoo.exceptions import ValidationError, UserError
 
 class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
@@ -19,3 +20,16 @@ class AccountMoveLine(models.Model):
         check_company=True,
         tracking=True,
     )
+
+    @api.constrains('account_id', 'tax_ids', 'tax_line_id', 'reconciled')
+    def _check_off_balance(self):
+        for line in self:
+            if line.is_budget:
+                continue
+            if line.account_id.internal_group == 'off_balance':
+                if any(a.internal_group != line.account_id.internal_group for a in line.move_id.line_ids.account_id):
+                    raise UserError(_('If you want to use "Off-Balance Sheet" accounts, all the accounts of the journal entry must be of this type'))
+                if line.tax_ids or line.tax_line_id:
+                    raise UserError(_('You cannot use taxes on lines with an Off-Balance account'))
+                if line.reconciled:
+                    raise UserError(_('Lines from "Off-Balance Sheet" accounts cannot be reconciled'))
