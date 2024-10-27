9125e6690a30d7475d9bbb5d18837a4208547797
He Jian <hej@eabax.com>
2024-10-27 11:46:36 +0800

    fix

--- l10n_cn_institution_reports/models/account_cash_flow_report.py
+++ l10n_cn_institution_reports/models/account_cash_flow_report.py
@@ -18,14 +18,30 @@ class CashFlowReportCustomHandler(models.AbstractModel):
         if aml_data['balance'] < 0:
             if aml_data['account_tag_id'] == tags_ids['operating_4001']:
                 self._add_report_data('received_operating_4001', aml_data, layout_data, report_data)
+            elif aml_data['account_tag_id'] == tags_ids['operating']:
+                self._add_report_data('paid_operating_activities', aml_data, layout_data, report_data)
+            else:
+                self._add_report_data('unclassified_activities_cash_out', aml_data, layout_data, report_data)
         elif aml_data['balance'] > 0:
-            d = 0
+            if aml_data['account_tag_id'] == tags_ids['operating']:
+                self._add_report_data('received_operating_activities', aml_data, layout_data, report_data)
+            else:
+                self._add_report_data('unclassified_activities_cash_in', aml_data, layout_data, report_data)
 
     def _get_layout_data(self):
         return {
             'opening_balance': {'name': '现金及现金等价物，期初', 'level': 0},
-            'operating_activities': {'name': '一、日常活动产生的现金流量', 'level': 0, 'parent_line_id': 'net_increase'},
-                'received_operating_4001': {'name': '财政基本支出拨款收到的现金', 'level': 2, 'parent_line_id': 'operating_activities'},
-            'net_increase': {'name': '五、现金净增加额', 'level': 0},
+            'net_increase': {'name': '现金净增加额', 'level': 0},
+                'operating_activities': {'name': '一、日常活动产生的现金流量', 'level': 2, 'parent_line_id': 'net_increase'},
+                    'operating_activities_cash_in': {'name': '日常活动的现金流入', 'level': 3, 'parent_line_id': 'operating_activities'},
+                        'received_operating_4001': {'name': '财政基本支出拨款收到的现金', 'level': 5, 'parent_line_id': 'operating_activities_cash_in'},
+                        'received_operating_activities': {'name': '事业活动收到的除财政拨款以外的现金', 'level': 5, 'parent_line_id': 'operating_activities_cash_in'},
+                    'operating_activities_cash_out': {'name': '日常活动的现金流出', 'level': 3, 'parent_line_id': 'operating_activities'},
+                        'paid_operating_activities': {'name': '购买商品、接受劳务支付的现金', 'level': 5, 'parent_line_id': 'operating_activities_cash_out'},
+                'investing_activities': {'name': '二、投资活动产生的现金流量', 'level': 2, 'parent_line_id': 'net_increase'},
+                'financing_activities': {'name': '三、筹资活动产生的现金流量', 'level': 2, 'parent_line_id': 'net_increase'},
+                'unclassified_activities': {'name': '未分类活动产生的现金流量', 'level': 2, 'parent_line_id': 'net_increase'},
+                    'unclassified_activities_cash_in': {'name': '现金流入', 'level': 3, 'parent_line_id': 'unclassified_activities'},
+                    'unclassified_activities_cash_out': {'name': '现金流出', 'level': 3, 'parent_line_id': 'unclassified_activities'},
             'closing_balance': {'name': '现金及现金等价物，期末余额', 'level': 0},
         }
