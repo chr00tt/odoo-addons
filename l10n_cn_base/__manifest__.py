4db12e59cc1db5f7906a3ce749ebb4b727be3b40
He Jian <hej@eabax.com>
2024-11-04 21:56:50 +0800

    fix

---
+++ l10n_cn_base/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': '中国 - 会计基础',
+    'author': "He Jian",
+    'category': 'Localization',
+    'website': 'https://github.com/chr00tt/odoo-addons',
+    'depends': ['l10n_cn'],
+    'data': [
+        'data/account.tax.group.csv',
+    ],
+    'license': 'LGPL-3',
+}
