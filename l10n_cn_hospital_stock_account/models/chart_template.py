7b49f5fa01e2ab2e69683c62943bd0cafbdb1422
He Jian <hej@eabax.com>
2024-11-11 21:53:57 +0800

    fix

--- l10n_cn_hospital_stock_account/models/chart_template.py
+++ l10n_cn_hospital_stock_account/models/chart_template.py
@@ -20,3 +20,31 @@ class AccountChartTemplate(models.Model):
             'property_valuation': 'real_time',
             'property_stock_valuation_account_id': AccountAccount.search([('code', '=', '1302.01')])
         })
+        self.env.ref("l10n_cn_hospital.product_category_21").with_company(company).write({
+            'property_valuation': 'real_time',
+            'property_stock_valuation_account_id': AccountAccount.search([('code', '=', '1302.02.01')])
+        })
+        self.env.ref("l10n_cn_hospital.product_category_22").with_company(company).write({
+            'property_valuation': 'real_time',
+            'property_stock_valuation_account_id': AccountAccount.search([('code', '=', '1302.02.02')])
+        })
+        self.env.ref("l10n_cn_hospital.product_category_23").with_company(company).write({
+            'property_valuation': 'real_time',
+            'property_stock_valuation_account_id': AccountAccount.search([('code', '=', '1302.02.03')])
+        })
+        self.env.ref("l10n_cn_hospital.product_category_24").with_company(company).write({
+            'property_valuation': 'real_time',
+            'property_stock_valuation_account_id': AccountAccount.search([('code', '=', '1302.02.04')])
+        })
+        self.env.ref("l10n_cn_hospital.product_category_25").with_company(company).write({
+            'property_valuation': 'real_time',
+            'property_stock_valuation_account_id': AccountAccount.search([('code', '=', '1302.02.05')])
+        })
+        self.env.ref("l10n_cn_hospital.product_category_3").with_company(company).write({
+            'property_valuation': 'real_time',
+            'property_stock_valuation_account_id': AccountAccount.search([('code', '=', '1302.03')])
+        })
+        self.env.ref("l10n_cn_hospital.product_category_4").with_company(company).write({
+            'property_valuation': 'real_time',
+            'property_stock_valuation_account_id': AccountAccount.search([('code', '=', '1302.04')])
+        })
