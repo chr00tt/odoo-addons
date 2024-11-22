7be16595e3d0b0d35ea95a0f6b597f6d433e42f4
He Jian <hej@eabax.com>
2024-11-22 11:05:01 +0800

    fix

--- account_analytic_report/models/account_analytic_report.py
+++ account_analytic_report/models/account_analytic_report.py
@@ -43,14 +43,14 @@ class AnalyticReportHandler(models.AbstractModel):
             raise UserError(_("Wrong ID for analytic line to expand: %s", line_dict_id))
 
 
-        partner_lines, totals_by_column_group = self._build_analytic_account_lines(report, options, record_id)
+        partner_lines, totals_by_column_group = self._build_analytic_account_lines(report, options, line_dict_id, record_id)
         lines = report._regroup_lines_by_name_prefix(options, partner_lines, '_report_expand_unfoldable_line_partner_ledger_prefix_group', 0)
 
         return {
             'lines': lines,
         }
 
-    def _build_analytic_account_lines(self, report, options, analytic_plan_id, level_shift=0):
+    def _build_analytic_account_lines(self, report, options, parent_line_id, analytic_plan_id, level_shift=0):
         lines = []
 
         totals_by_column_group = {
@@ -61,23 +61,23 @@ class AnalyticReportHandler(models.AbstractModel):
             for column_group_key in options['column_groups']
         }
 
-        for partner, results in self._query_analytic_accounts(options, analytic_plan_id):
-            if not partner:
+        for analytic_account, results in self._query_analytic_accounts(options, analytic_plan_id):
+            if not analytic_account:
                 continue
 
-            partner_values = defaultdict(dict)
+            analytic_account_values = defaultdict(dict)
             for column_group_key in options['column_groups']:
-                partner_sum = results.get(column_group_key, {})
+                analytic_account_sum = results.get(column_group_key, {})
 
-                partner_values[column_group_key]['debit'] = partner_sum.get('debit', 0.0)
-                partner_values[column_group_key]['credit'] = partner_sum.get('credit', 0.0)
-                partner_values[column_group_key]['balance'] = partner_sum.get('balance', 0.0)
+                analytic_account_values[column_group_key]['debit'] = analytic_account_sum.get('debit', 0.0)
+                analytic_account_values[column_group_key]['credit'] = analytic_account_sum.get('credit', 0.0)
+                analytic_account_values[column_group_key]['balance'] = analytic_account_sum.get('balance', 0.0)
 
-                totals_by_column_group[column_group_key]['debit'] += partner_values[column_group_key]['debit']
-                totals_by_column_group[column_group_key]['credit'] += partner_values[column_group_key]['credit']
-                totals_by_column_group[column_group_key]['balance'] += partner_values[column_group_key]['balance']
+                totals_by_column_group[column_group_key]['debit'] += analytic_account_values[column_group_key]['debit']
+                totals_by_column_group[column_group_key]['credit'] += analytic_account_values[column_group_key]['credit']
+                totals_by_column_group[column_group_key]['balance'] += analytic_account_values[column_group_key]['balance']
 
-            lines.append(self._get_report_line_analytic_accounts(options, partner, partner_values, level_shift=level_shift))
+            lines.append(self._get_report_line_analytic_accounts(options, analytic_account, analytic_account_values, parent_line_id, level_shift=level_shift))
 
         return lines, totals_by_column_group
 
@@ -100,30 +100,9 @@ class AnalyticReportHandler(models.AbstractModel):
         for res in self._cr.dictfetchall():
             assign_sum(res)
 
-        # Correct the sums per partner, for the lines without partner reconciled with a line having a partner
-        query, params = self._get_sums_without_partner(options)
-
-        self._cr.execute(query, params)
         totals = {}
         for total_field in ['debit', 'credit', 'balance']:
             totals[total_field] = {col_group_key: 0 for col_group_key in options['column_groups']}
-
-        for row in self._cr.dictfetchall():
-            totals['debit'][row['column_group_key']] += row['debit']
-            totals['credit'][row['column_group_key']] += row['credit']
-            totals['balance'][row['column_group_key']] += row['balance']
-
-            if row['groupby'] not in groupby_partners:
-                continue
-
-            assign_sum(row)
-
-        if None in groupby_partners:
-            # Debit/credit are inverted for the unknown partner as the computation is made regarding the balance of the known partner
-            for column_group_key in options['column_groups']:
-                groupby_partners[None][column_group_key]['debit'] += totals['credit'][column_group_key]
-                groupby_partners[None][column_group_key]['credit'] += totals['debit'][column_group_key]
-                groupby_partners[None][column_group_key]['balance'] -= totals['balance'][column_group_key]
 
         # Retrieve the partners to browse.
         # groupby_partners.keys() contains all account ids affected by:
@@ -177,12 +156,29 @@ class AnalyticReportHandler(models.AbstractModel):
 
         return ' UNION ALL '.join(queries), params
 
-    def _get_report_line_analytic_account(self, options, parent_line_id, analytic_account, level_shift=0):
+    def _get_report_line_analytic_accounts(self, options, analytic_account, analytic_account_values, parent_line_id, level_shift=0):
         company_currency = self.env.company.currency_id
         unfold_all = (self._context.get('print_mode') and not options.get('unfolded_lines')) or options.get('unfold_all')
 
+        unfoldable = False
         column_values = []
         report = self.env['account.report']
+        for column in options['columns']:
+            col_expr_label = column['expression_label']
+            value = analytic_account_values[column['column_group_key']].get(col_expr_label)
+
+            if col_expr_label in {'debit', 'credit', 'balance'}:
+                formatted_value = report.format_value(value, figure_type=column['figure_type'], blank_if_zero=column['blank_if_zero'])
+            else:
+                formatted_value = report.format_value(value, figure_type=column['figure_type']) if value is not None else value
+
+            unfoldable = unfoldable or (col_expr_label in ('debit', 'credit') and not company_currency.is_zero(value))
+
+            column_values.append({
+                'name': formatted_value,
+                'no_format': value,
+                'class': 'number'
+            })
 
         line_id = report._get_generic_line_id('account.analytic.account', analytic_account.id) if analytic_account else report._get_generic_line_id('account.analytic.account', None, markup='no_analytic_account')
 
@@ -190,7 +186,7 @@ class AnalyticReportHandler(models.AbstractModel):
             'id': line_id,
             'name': analytic_account is not None and (analytic_account.name or '')[:128] or self._get_no_analytic_account_line_label(),
             'columns': column_values,
-            'level': 2 + level_shift,
+            'level': 4 + level_shift,
             'parent_id': parent_line_id,
             'unfoldable': True,
             'unfolded': line_id in options['unfolded_lines'] or unfold_all,
