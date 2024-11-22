dcd4b19f7b0c980d98108af1c23e12b330db1674
He Jian <hej@eabax.com>
2024-11-22 14:08:42 +0800

    fix

--- account_analytic_report/models/account_analytic_ledger.py
+++ account_analytic_report/models/account_analytic_ledger.py
@@ -265,7 +265,167 @@ class AnalyticLedgerCustomHandler(models.AbstractModel):
 
         return init_balance_by_col_group
 
+    def _get_aml_values(self, options, analytic_account_ids, offset=0, limit=None):
+        rslt = {analytic_account_id: [] for analytic_account_id in analytic_account_ids}
 
+        analytic_account_ids_wo_none = [x for x in analytic_account_ids if x]
+        directly_linked_aml_analytic_account_clauses = []
+        directly_linked_aml_analytic_account_params = []
+        indirectly_linked_aml_analytic_account_params = []
+        indirectly_linked_aml_analytic_account_clause = 'aml_with_analytic_account.analytic_account_id IS NOT NULL'
+        if None in analytic_account_ids:
+            directly_linked_aml_analytic_account_clauses.append('account_move_line.analytic_account_id IS NULL')
+        if analytic_account_ids_wo_none:
+            directly_linked_aml_analytic_account_clauses.append('account_move_line.analytic_account_id IN %s')
+            directly_linked_aml_analytic_account_params.append(tuple(analytic_account_ids_wo_none))
+            indirectly_linked_aml_analytic_account_clause = 'aml_with_analytic_account.analytic_account_id IN %s'
+            indirectly_linked_aml_analytic_account_params.append(tuple(analytic_account_ids_wo_none))
+        directly_linked_aml_analytic_account_clause = '(' + ' OR '.join(directly_linked_aml_analytic_account_clauses) + ')'
+
+        ct_query = self.env['res.currency']._get_query_currency_table(options)
+        queries = []
+        all_params = []
+        lang = self.env.lang or get_lang(self.env).code
+        journal_name = f"COALESCE(journal.name->>'{lang}', journal.name->>'en_US')" if \
+            self.pool['account.journal'].name.translate else 'journal.name'
+        account_name = f"COALESCE(account.name->>'{lang}', account.name->>'en_US')" if \
+            self.pool['account.account'].name.translate else 'account.name'
+        report = self.env.ref('account_analytic_report.analytic_ledger_report')
+        for column_group_key, group_options in report._split_options_per_column_group(options).items():
+            tables, where_clause, where_params = report._query_get(group_options, 'strict_range')
+
+            all_params += [
+                column_group_key,
+                *where_params,
+                *directly_linked_aml_analytic_account_params,
+                column_group_key,
+                *indirectly_linked_aml_analytic_account_params,
+                *where_params,
+                group_options['date']['date_from'],
+                group_options['date']['date_to'],
+            ]
+
+            # For the move lines directly linked to this analytic_account
+            queries.append(f'''
+                SELECT
+                    account_move_line.id,
+                    account_move_line.date,
+                    account_move_line.date_maturity,
+                    account_move_line.name,
+                    account_move_line.ref,
+                    account_move_line.company_id,
+                    account_move_line.account_id,
+                    account_move_line.payment_id,
+                    account_move_line.analytic_account_id,
+                    account_move_line.currency_id,
+                    account_move_line.amount_currency,
+                    account_move_line.matching_number,
+                    ROUND(account_move_line.debit * currency_table.rate, currency_table.precision)   AS debit,
+                    ROUND(account_move_line.credit * currency_table.rate, currency_table.precision)  AS credit,
+                    ROUND(account_move_line.balance * currency_table.rate, currency_table.precision) AS balance,
+                    account_move.name                                                                AS move_name,
+                    account_move.move_type                                                           AS move_type,
+                    account.code                                                                     AS account_code,
+                    {account_name}                                                                   AS account_name,
+                    journal.code                                                                     AS journal_code,
+                    {journal_name}                                                                   AS journal_name,
+                    %s                                                                               AS column_group_key,
+                    'directly_linked_aml'                                                            AS key,
+                    0                                                                                AS partial_id
+                FROM {tables}
+                JOIN account_move ON account_move.id = account_move_line.move_id
+                LEFT JOIN {ct_query} ON currency_table.company_id = account_move_line.company_id
+                LEFT JOIN res_company company               ON company.id = account_move_line.company_id
+                LEFT JOIN res_analytic_account analytic_account               ON analytic_account.id = account_move_line.analytic_account_id
+                LEFT JOIN account_account account           ON account.id = account_move_line.account_id
+                LEFT JOIN account_journal journal           ON journal.id = account_move_line.journal_id
+                WHERE {where_clause} AND {directly_linked_aml_analytic_account_clause}
+                ORDER BY account_move_line.date, account_move_line.id
+            ''')
+
+            # For the move lines linked to no analytic_account, but reconciled with this analytic_account. They will appear in grey in the report
+            queries.append(f'''
+                SELECT
+                    account_move_line.id,
+                    account_move_line.date,
+                    account_move_line.date_maturity,
+                    account_move_line.name,
+                    account_move_line.ref,
+                    account_move_line.company_id,
+                    account_move_line.account_id,
+                    account_move_line.payment_id,
+                    aml_with_analytic_account.analytic_account_id,
+                    account_move_line.currency_id,
+                    account_move_line.amount_currency,
+                    account_move_line.matching_number,
+                    CASE WHEN aml_with_analytic_account.balance > 0 THEN 0 ELSE ROUND(
+                        partial.amount * currency_table.rate, currency_table.precision
+                    ) END                                                                               AS debit, 
+                    CASE WHEN aml_with_analytic_account.balance < 0 THEN 0 ELSE ROUND(
+                        partial.amount * currency_table.rate, currency_table.precision
+                    ) END                                                                               AS credit, 
+                    - sign(aml_with_analytic_account.balance) * ROUND(
+                        partial.amount * currency_table.rate, currency_table.precision
+                    )                                                                                   AS balance, 
+                    account_move.name                                                                   AS move_name,
+                    account_move.move_type                                                              AS move_type,
+                    account.code                                                                        AS account_code,
+                    {account_name}                                                                      AS account_name,
+                    journal.code                                                                        AS journal_code,
+                    {journal_name}                                                                      AS journal_name,
+                    %s                                                                                  AS column_group_key,
+                    'indirectly_linked_aml'                                                             AS key,
+                    partial.id                                                                          AS partial_id
+                FROM {tables}
+                    LEFT JOIN {ct_query} ON currency_table.company_id = account_move_line.company_id,
+                    account_partial_reconcile partial,
+                    account_move,
+                    account_move_line aml_with_analytic_account,
+                    account_journal journal,
+                    account_account account
+                WHERE
+                    (account_move_line.id = partial.debit_move_id OR account_move_line.id = partial.credit_move_id)
+                    AND account_move_line.analytic_account_id IS NULL
+                    AND account_move.id = account_move_line.move_id
+                    AND (aml_with_analytic_account.id = partial.debit_move_id OR aml_with_analytic_account.id = partial.credit_move_id)
+                    AND {indirectly_linked_aml_analytic_account_clause}
+                    AND journal.id = account_move_line.journal_id
+                    AND account.id = account_move_line.account_id
+                    AND {where_clause}
+                    AND partial.max_date BETWEEN %s AND %s
+                ORDER BY account_move_line.date, account_move_line.id
+            ''')
+
+        query = '(' + ') UNION ALL ('.join(queries) + ')'
+
+        if offset:
+            query += ' OFFSET %s '
+            all_params.append(offset)
+
+        if limit:
+            query += ' LIMIT %s '
+            all_params.append(limit)
+
+        self._cr.execute(query, all_params)
+        for aml_result in self._cr.dictfetchall():
+            if aml_result['key'] == 'indirectly_linked_aml':
+
+                # Append the line to the analytic_account found through the reconciliation.
+                if aml_result['analytic_account_id'] in rslt:
+                    rslt[aml_result['analytic_account_id']].append(aml_result)
+
+                # Balance it with an additional line in the Unknown analytic_account section but having reversed amounts.
+                if None in rslt:
+                    rslt[None].append({
+                        **aml_result,
+                        'debit': aml_result['credit'],
+                        'credit': aml_result['debit'],
+                        'balance': -aml_result['balance'],
+                    })
+            else:
+                rslt[aml_result['analytic_account_id']].append(aml_result)
+
+        return rslt
 
     def _report_expand_unfoldable_line_analytic_account_ledger(self, line_dict_id, groupby, options, progress, offset, unfold_all_batch_data=None):
         def init_load_more_progress(line_dict):
