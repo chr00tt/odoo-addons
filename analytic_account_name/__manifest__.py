40dda5db45e2c19d89da726954ca79b30805d94e
He Jian <hej@eabax.com>
2024-12-17 17:46:11 +0800

    fix

--- analytic_account_name/__manifest__.py
+++ analytic_account_name/__manifest__.py
@@ -5,7 +5,7 @@
     'summary': '在凭证分录里，不易区分分析科目属于哪个分析计划，所以把计划名称作为分析科目名称的前缀。',
     'category': 'Accounting/Accounting',
     'website': 'https://github.com/chr00tt/odoo-addons',
-    'depends': ['analytic'],
+    'depends': ['account_analytic_parent'],
     'auto_install': True,
     'license': 'LGPL-3',
 }
