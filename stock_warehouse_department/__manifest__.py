1f22f546ee2d16664635952dcefed5a8d841e918
He Jian <hej@eabax.com>
2025-02-11 16:33:30 +0800

    Add stock_warehouse_department

---
+++ stock_warehouse_department/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Stock Warehouse Department',
+    'author': 'He Jian',
+    'website': 'https://github.com/chr00tt/odoo-addons',
+    'depends': ['stock', 'hr'],
+    'category': 'Inventory/Inventory',
+    'data': [
+        'views/stock_warehouse_views.xml',
+    ],
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
