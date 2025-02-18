aec1c29ff1d6e774fb89fa7488f4c26692555fe0
He Jian <hej@eabax.com>
2025-02-18 15:54:15 +0800

    fix

--- properties_definition/__manifest__.py
+++ properties_definition/__manifest__.py
@@ -3,16 +3,14 @@
 {
     'name': 'Properties definition',
     'author': 'He Jian',
-    'version': '1.10',
     'category': 'Hidden',
-    'website': 'https://github.com/chr00tt/odoo-addons',
     'sequence': 145,
     'description': """
 Properties definition for Odoo field.
 =====================================
 Enter the definition if field ttype is properties on field edit view.
     """,
-    'website': 'https://github.com/chr00tt/properties_definition',
+    'website': 'https://github.com/chr00tt/odoo-addons',
     'depends': ['base'],
     'data': [
         'views/ir_model_views.xml',
---
+++ stock_barcode_production_date/models/stock_move_line.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models
+
+
+class StockMoveLine(models.Model):
+    _inherit = 'stock.move.line'
+
+    production_date = fields.Datetime(
+        string='生产日期', compute='_compute_production_date', store=True)
+
+    @api.depends('product_id', 'lot_id.production_date')
+    def _compute_production_date(self):
+        for move_line in self:
+            if move_line.lot_id.production_date:
+                move_line.production_date = move_line.lot_id.production_date
+            else:
+                move_line.production_date = False
+
+    def _get_fields_stock_barcode(self):
+        return super()._get_fields_stock_barcode() + ['production_date']
+
