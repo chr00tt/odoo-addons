4949da2376bc2f9493b9c1488545d83909192c46
He Jian <hej@eabax.com>
2025-03-10 16:02:54 +0800

    fix

--- nhsa_consumables/models/product_template.py
+++ nhsa_consumables/models/product_template.py
@@ -6,11 +6,9 @@ from odoo import api, fields, models, tools, _, SUPERUSER_ID
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-    nhsa_consumables_id = fields.Many2one(
-        'nhsa.consumables', '医保代码',
-        required=True)
+    nhsa_consumables_id = fields.Many2one('nhsa.consumables', '医保代码')
 
-    nhsa_consumables_categ_id = fields.Many2one(related='nhsa_consumables_id.nhsa_consumables_categ_id')
+    nhsa_consumables_categ_id = fields.Many2one(related='nhsa_consumables_id.nhsa_consumables_categ_id', store=True)
     common_name = fields.Char(related='nhsa_consumables_id.common_name')
     material = fields.Char(related='nhsa_consumables_id.material')
     specifications = fields.Char(related='nhsa_consumables_id.specifications')
