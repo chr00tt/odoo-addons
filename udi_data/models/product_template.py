1aef3398a14fa89c3ba284689025d5c283b2bb97
He Jian <hej@eabax.com>
2025-03-11 16:53:19 +0800

    fix

--- udi_data/models/product_template.py
+++ udi_data/models/product_template.py
@@ -8,9 +8,9 @@ class ProductTemplate(models.Model):
 
     udi_data_id = fields.Many2one('udi.data', '医疗器械唯一标识')
 
-    udi_zxxsdycpbs = fields.Char("最小销售单元产品标识")
-    udi_sydycpbs = fields.Char("使用单元产品标识")
-    udi_btcpbs = fields.Char("本体产品标识")
-    udi_cpmctymc = fields.Char("产品名称/通用名称")
-    udi_spmc = fields.Char("商品名称")
-    udi_ggxh = fields.Char("型号规格/包装规格")
+    udi_zxxsdycpbs = fields.Char(related='udi_data_id.zxxsdycpbs')
+    udi_sydycpbs = fields.Char(related='udi_data_id.sydycpbs')
+    udi_btcpbs = fields.Char(related='udi_data_id.btcpbs')
+    udi_cpmctymc = fields.Char(related='udi_data_id.cpmctymc')
+    udi_spmc = fields.Char(related='udi_data_id.spmc')
+    udi_ggxh = fields.Char(related='udi_data_id.ggxh')
