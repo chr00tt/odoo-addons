bc068f3707aa5d5aed6235b52eb1f34848d03622
He Jian <hej@eabax.com>
2025-03-14 12:15:56 +0800

    fix

--- udi_data/models/product_template.py
+++ udi_data/models/product_template.py
@@ -25,10 +25,39 @@ class ProductTemplate(models.Model):
     @api.onchange('udi_data_id')
     def _onchange_udi_data_id(self):
         if self.udi_data_id:
-            # 如果有医保耗材分类编码，并且未设置医保医用耗材分类与代码
+            # 设置医用耗材标志
+            self.is_medical_consumables = self.udi_data_id.cplb == '耗材'
+
+            # 设置医保医用耗材分类与代码
             if self.udi_data_id.ybbm and not self.nhsa_consumables_id:
                 self.nhsa_consumables_id = self.udi_data_id.nhsa_consumables_id
 
-            # 如果有最小销售单元产品标识，并且未设置条码
-            if self.udi_data_id.zxxsdycpbs and not self.barcode:
-                self.barcode = self.udi_data_id.zxxsdycpbs
+            # 设置条码
+            if not self.barcode:
+                self.barcode = self.udi_data_id.sydycpbs if self.udi_data_id.sydycpbs else self.udi_data_id.zxxsdycpbs
+
+            # 设置产品包装
+            if self.udi_data_id.sydycpbs:
+                product_packaging = self.env['product.packaging'].search([('barcode', '=', self.udi_data_id.zxxsdycpbs)], limit=1)
+                if not product_packaging:
+                    product_packaging = self.env['product.packaging'].create({
+                        'name': '%d %s' % (self.udi_data_id.zxxsdyzsydydsl, self.uom_id.name),
+                        'product_id': self.product_variant_id.id,
+                        'qty': self.udi_data_id.zxxsdyzsydydsl,
+                        'barcode': self.udi_data_id.zxxsdycpbs,
+                    })
+
+    @api.onchange('barcode')
+    def _barcode_to_udi_data(self):
+        # 设置医疗器械唯一标识
+        if self.barcode and not self.udi_data_id:
+            udi_data = self.env['udi.data'].search(['|', ('zxxsdycpbs', '=', self.barcode), ('sydycpbs', '=', self.barcode)], limit=1)
+            if udi_data:
+                self.udi_data_id = udi_data.id
+            else:
+                return {
+                    'warning': {
+                        'title': '警告',
+                        'message': '未能在医疗器械唯一标识数据库里找到产品标识 %s' % self.barcode,
+                    }
+                }
