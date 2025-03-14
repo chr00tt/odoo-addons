2ee5b27abeb1007681d77e291c4804cd6c5eea14
He Jian <hej@eabax.com>
2025-03-14 17:40:49 +0800

    fix

--- udi_data/models/rss_mixin.py
+++ udi_data/models/rss_mixin.py
@@ -14,6 +14,7 @@ import re
 UDID_DAILY_RSS_URL = "https://udi.nmpa.gov.cn/rss/download.html?files=daily"
 UDID_WEEKLY_RSS_URL = "https://udi.nmpa.gov.cn/rss/download.html?files=weekly"
 UDID_MONTHLY_RSS_URL = "https://udi.nmpa.gov.cn/rss/download.html?files=monthly"
+UDID_FULL_RSS_URL = "https://udi.nmpa.gov.cn/rss/download.html?files=full"
 
 class RssMixin(models.AbstractModel):
     _name = 'rss.mixin'
@@ -27,6 +28,9 @@ class RssMixin(models.AbstractModel):
 
     def do_monthly_update(self):
         self._do_update(UDID_MONTHLY_RSS_URL)
+
+    def do_full_update(self):
+        self._do_update(UDID_FULL_RSS_URL)
 
     def _do_update(self, rss_url):
         try:
