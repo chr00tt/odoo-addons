e1371daa90f1fd38bd224830cf498f2e2738e62c
He Jian <hej@eabax.com>
2025-03-14 18:02:29 +0800

    fix

--- udi_data/__manifest__.py
+++ udi_data/__manifest__.py
@@ -17,10 +17,10 @@
         'data/ir_cron_data.xml',
         'data/udi_data_data.xml',
         'data/medical.device.category.csv',
-        'views/medical_device_category_views.xml',
         'views/product_views.xml',
         'views/udi_data_views.xml',
         'views/stock_menu_views.xml',
+        'views/medical_device_category_views.xml',
     ],
     'license': 'LGPL-3',
 }
