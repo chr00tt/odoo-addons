f58ea882890de5cd6b1d4fb98f5913fb79b870d0
He Jian <hej@eabax.com>
2025-03-18 16:36:17 +0800

    fix

--- nhsa_consumables/models/nhsa_consumables.py
+++ nhsa_consumables/models/nhsa_consumables.py
@@ -19,3 +19,26 @@ class NHSAConsumables(models.Model):
     material = fields.Char('材质', required=True)
     specifications = fields.Char('规格', required=True)
     enterprise = fields.Char('企业', required=True)
+
+    product_count = fields.Integer(
+        '# 产品', compute='_compute_product_count')
+    supplier_count = fields.Integer(
+        '# 供应', compute='_compute_supplier_count')
+
+    def _compute_product_count(self):
+        read_group_res = self.env['product.template'].read_group([('nhsa_consumables_id', 'in', self.ids)], ['nhsa_consumables_id'], ['nhsa_consumables_id'])
+        group_data = dict((data['nhsa_consumables_id'][0], data['nhsa_consumables_id_count']) for data in read_group_res)
+        for categ in self:
+            product_count = 0
+            for sub_categ_id in categ.search([('id', 'in', categ.ids)]).ids:
+                product_count += group_data.get(sub_categ_id, 0)
+            categ.product_count = product_count
+
+    def _compute_supplier_count(self):
+        read_group_res = self.env['product.supplierinfo'].read_group([('nhsa_consumables_id', 'in', self.ids)], ['nhsa_consumables_id'], ['nhsa_consumables_id'])
+        group_data = dict((data['nhsa_consumables_id'][0], data['nhsa_consumables_id_count']) for data in read_group_res)
+        for categ in self:
+            supplier_count = 0
+            for sub_categ_id in categ.search([('id', 'child_of', categ.ids)]).ids:
+                supplier_count += group_data.get(sub_categ_id, 0)
+            categ.supplier_count = supplier_count
