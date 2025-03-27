106554ea27b141bf4ef1f50a3623dc52b66b19f2
He Jian <hej@eabax.com>
2025-03-27 15:23:22 +0800

    add

---
+++ l10n_cn_standard/README.md
@@ -0,0 +1,4 @@
+# l10n_cn_standard
+China - Standard CoA
+
+符合中国企业会计准则的科目设置。
---
+++ l10n_cn_standard/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': '中国 - 企业会计准则',
+    'icon': '/account/static/description/l10n.png',
+    'countries': ['cn'],
+    'category': 'Accounting/Localizations/Account Charts',
+    'author': 'He Jian',
+    'website': 'https://github.com/chr00tt/l10n_cn_standard',
+    'depends': [
+        'account',
+    ],
+    'demo': [
+        'data/demo_company.xml',
+    ],
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
