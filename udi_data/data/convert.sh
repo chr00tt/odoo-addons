84162667b6060f5cc3730d24f39c62f1088becef
He Jian <hej@eabax.com>
2025-04-15 19:09:04 +0800

    fix

--- udi_data/data/convert.sh
+++ udi_data/data/convert.sh
@@ -17,5 +17,5 @@ awk -F ',' 'NR>4 {gsub(/^"|"$/, "", $2); split($2, lb, " "); print "medical_devi
 awk -v FPAT='([^,]+)|("[^"]+")' 'NR>4 {gsub(/^"|"$/, "", $1); gsub(/^"|"$/, "", $2); split($2, lb, " "); print "medical_device_category_6840_old_" $1 "," $3 ",6840-" $1 ",medical_device_category_6840_old_" lb[1] ",," $4 ",," $5}' 体外诊断试剂分类目录\(6840-2013\).csv >> medical.device.category.csv
 
 # 体外诊断试剂分类目录
-awk -v FPAT='([^,]*)|("[^"]*")' -v OFS=, 'NR>1 {print "medical_device_category_6840_" $2 "," $3 ",6840-" $2 ",medical_device_category_6840,,,," $7}' 体外诊断试剂分类目录.csv | uniq >> medical.device.category.csv
-awk -v FPAT='([^,]*)|("[^"]*")' -v OFS=, 'NR>1 {print "medical_device_category_6840_" $2 "-" $4 "," $5 ",6840-" $2 "-" $4 ",medical_device_category_6840_" $2 ",," $6 ",," $7}' 体外诊断试剂分类目录.csv >> medical.device.category.csv
+awk -v FPAT='([^,]*)|("[^"]*")' -v OFS=, 'NR>1 {f2 = sprintf("%02d", $2); print "medical_device_category_6840_" $2 "," $3 ",6840-" f2 ",medical_device_category_6840,,,," $7}' 体外诊断试剂分类目录.csv | uniq >> medical.device.category.csv
+awk -v FPAT='([^,]*)|("[^"]*")' -v OFS=, 'NR>1 {f2 = sprintf("%02d", $2); f4 = sprintf("%05d", $4); print "medical_device_category_6840_" $2 "-" $4 "," $5 ",6840-" f2 "-" f4 ",medical_device_category_6840_" $2 ",," $6 ",," $7}' 体外诊断试剂分类目录.csv >> medical.device.category.csv
