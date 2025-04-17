eb6d2c79d870dea2b932c9646e231435a8aca3b4
He Jian <hej@eabax.com>
2025-04-17 16:02:57 +0800

    fix

--- base_import_async_plus/models/base_import_import.py
+++ base_import_async_plus/models/base_import_import.py
@@ -8,16 +8,18 @@ class BaseImportImport(models.TransientModel):
     _inherit = "base_import.import"
 
     def _import_one_chunk(self, model_name, attachment, options):
+        model = self.env[self.res_model].with_context(options.get('context', {}))
+
         name_create_enabled_fields = options.pop('name_create_enabled_fields', {})
         import_limit = options.pop('limit', None)
-        model_obj = self.env[model_name].with_context(
+        model = model.with_context(
             import_file=True,
             name_create_enabled_fields=name_create_enabled_fields,
             import_set_empty_fields=options.get('import_set_empty_fields', []),
             import_skip_records=options.get('import_skip_records', []),
             _import_limit=import_limit)
         fields, data = self._read_csv_attachment(attachment, options)
-        result = model_obj.load(fields, data)
+        result = model.load(fields, data)
         error_message = [
             message["message"]
             for message in result["messages"]
