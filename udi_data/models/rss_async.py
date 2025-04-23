3edfc5d2d69321ec7d2ecfbd7b7438be94044923
He Jian <hej@eabax.com>
2025-04-23 10:43:40 +0800

    fix

--- udi_data/models/rss_async.py
+++ udi_data/models/rss_async.py
@@ -7,12 +7,13 @@ import os
 class RssMixin(models.AbstractModel):
     _inherit = 'rss.mixin'
 
+    # 异步导入数据文件
     def _import_data_files(self, directory):
         for root, _, files in os.walk(directory):
             for file in files:
                 description = '从 %s 导入唯一标识' % file
                 if file.endswith('.xml'):
-                    self.with_delay(description=description, priority=300)._import_xml(os.path.join(root, file))
+                    self.with_delay(description=description, priority=1000)._import_xml(os.path.join(root, file))
                 elif file.endswith('.zip'):
                     extract_dir = self._extract_zip(directory + '/' + file)
-                    self.with_delay(description=description, priority=300)._import_data_files(extract_dir)
+                    self.with_delay(description=description, priority=1000)._import_data_files(extract_dir)
