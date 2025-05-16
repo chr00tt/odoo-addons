c845c97e9b58e750832ebd478458af0063a7b008
He Jian <hej@eabax.com>
2025-05-16 11:38:56 +0800

    [IMP] stock_request_tire_validation: 检查是否需要审批.

--- stock_request_tier_validation/models/stock_request_order.py
+++ stock_request_tier_validation/models/stock_request_order.py
@@ -9,3 +9,13 @@ class StockRequestOrder(models.Model):
     _state_to = ["open"]
 
     _tier_validation_manual_config = False
+
+    def action_confirm(self):
+        vals = {"state": "open"}
+
+        # 检查是否需要审批
+        self._tier_validation_check_state_on_write(vals)
+        self._tier_validation_check_write_allowed(vals)
+        self._tier_validation_check_write_remove_reviews(vals)
+
+        return super().action_confirm()
