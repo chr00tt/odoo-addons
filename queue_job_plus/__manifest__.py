75e9658a390f5f9bc422638ab97b74639b1a6edf
He Jian <hej@eabax.com>
2025-05-23 17:08:23 +0800

    fix

---
+++ queue_job_plus/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "Job Queue plus",
+    'author': 'He Jian',
+    'website': 'https://github.com/chr00tt/odoo-addons',
+    "license": "LGPL-3",
+    "category": "Generic Modules",
+    "depends": ["queue_job"],
+    "data": [
+        "views/queue_job_views.xml",
+    ],
+    "auto_install": True,
+}
