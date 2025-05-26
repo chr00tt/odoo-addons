9f4188740328d3f086b9c1e10816218f324d74fe
He Jian <hej@eabax.com>
2025-05-26 16:48:07 +0800

    fix

--- udi_data/__manifest__.py
+++ udi_data/__manifest__.py
@@ -18,7 +18,7 @@
         'security/ir.model.access.csv',
         'data/ir_cron_data.xml',
         'data/udi_data_data.xml',
-        # 'data/medical.device.category.csv', # 采用异步导入
+        'data/medical.device.category.csv', # 采用异步导入
         'views/product_views.xml',
         'views/udi_data_views.xml',
         'views/medical_device_category_views.xml',
