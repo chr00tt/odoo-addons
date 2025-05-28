9ff0c1db25827d825cfff4c5b6304545c941996b
He Jian <hej@eabax.com>
2025-05-28 11:41:48 +0800

    [ADD] product_certification.

--- product_certification/__manifest__.py
+++ product_certification/__manifest__.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 
 {
-    'name': '联系人资质',
+    'name': '产品资质',
     'author': 'He Jian',
     'website': 'https://github.com/chr00tt/odoo-addons',
     'depends': [
-        'base',
+        'product',
 
         'certification',
     ],
     'category': 'Inventory/Inventory',
     'data': [
-        'views/res_partner_views.xml',
+        'views/product_template_views.xml',
     ],
     'license': 'LGPL-3',
 }
---
+++ product_certification/models/product_template.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models, tools, _, SUPERUSER_ID
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    certificate_ids = fields.One2many('certificate', 'res_id', '资质')
+    certificate_count = fields.Integer(compute='_compute_certificate_count', string='资质数量')
+
+    def _compute_certificate_count(self):
+        for record in self:
+            record.certificate_count = record.env['certificate'].search_count([
+                ('res_model', '=', 'product.template'),
+                ('res_id', '=', record.id),
+            ])
+
+    def action_open_certificates(self):
+        self.ensure_one()
+        return {
+            'type': 'ir.actions.act_window',
+            'name': '资质',
+            'res_model': 'certificate',
+            'view_mode': 'kanban,tree,form',
+            'context': {
+                'default_res_model': self._name,
+                'default_res_id': self.id,
+                'default_company_id': self.company_id.id,
+            },
+            'domain': [('res_model', '=', 'product.template'), ('res_id', '=', self.id)],
+        }
