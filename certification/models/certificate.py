d510f3dcf7343908e435e395ee74dd9761771455
He Jian <hej@eabax.com>
2025-06-03 11:01:24 +0800

    fix

--- certification/models/certificate.py
+++ certification/models/certificate.py
@@ -14,5 +14,5 @@ class Certificate(models.Model):
     certificate_number = fields.Char(string='证书编号')
     issue_date = fields.Date(string='发证日期')
     expiry_date = fields.Date(string='到期日期')
-    ir_attachment_id = fields.Many2one('ir.attachment', string='资质文件', ondelete='cascade')
+    ir_attachment_id = fields.Many2one('ir.attachment', string='资质文件', required=True, ondelete='cascade')
     active = fields.Boolean(string='有效', default=True)
