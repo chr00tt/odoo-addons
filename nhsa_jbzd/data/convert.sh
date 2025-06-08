b900f55567fd55568a3427bd895b9af5c5f957e0
He Jian <hej@eabax.com>
2025-06-08 17:43:49 +0800

    fix

---
+++ nhsa_jbzd/data/convert.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo 'id,code,master_code,name,category' > nhsa.jbzd.csv
+awk -F ',' 'NR>1 {code = $1 ? $1 : $2; id = code; gsub(/\./, "_", id); gsub(/"/, "", id); master = $1 ? "True" : "False"; print "nhsa_jbzd_" id "," code "," master "," $3 "," $4}' 手术操作分类代码国家临床版3.0.csv >> nhsa.jbzd.csv
