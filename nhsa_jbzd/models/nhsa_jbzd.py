399e2f35219f0628381606494dd845745be860b6
He Jian <hej@eabax.com>
2025-06-09 08:29:22 +0800

    fix

--- nhsa_jbzd/models/nhsa_jbzd.py
+++ nhsa_jbzd/models/nhsa_jbzd.py
@@ -6,7 +6,7 @@ class NhsaJbzd(models.Model):
     _name = 'nhsa.jbzd'
     _description = '手术操作代码'
 
-    code = fields.Char('编码', True, required=True)
+    code = fields.Char('编码', index=True, required=True)
     master_code = fields.Boolean('主要编码', default=False)
     name = fields.Char('名称', index='trigram', required=True)
     category = fields.Selection([
