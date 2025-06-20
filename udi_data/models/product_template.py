0059d471e66344af9c2eff9909e891efa0c8206d
He Jian <hej@eabax.com>
2025-06-20 15:34:00 +0800

    [IMP] udi_data: 添加产品来源字段.

--- udi_data/models/product_template.py
+++ udi_data/models/product_template.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import api, fields, models, tools, _, SUPERUSER_ID
+from odoo import api, fields, models
 
 class ProductTemplate(models.Model):
     _inherit = "product.template"
@@ -8,8 +8,12 @@ class ProductTemplate(models.Model):
     udi_data_id = fields.Many2one('udi.data', '医疗器械唯一标识', compute='_compute_udi_data_id', store=True)
 
     zczbhhzbapzbh = fields.Char("注册证编号或者备案凭证编号")
+    product_origin = fields.Selection([
+        ('domestic', '国产'),
+        ('imported', '进口'),
+    ], string='产品来源', default='domestic')
     syqsfxyjxmj = fields.Boolean("使用前是否需要进行灭菌")
-    gllb = fields.Selection([('1', 'Ⅰ'), ('2', 'Ⅱ'), ('3', 'Ⅲ')], string='管理类别')
+    gllb = fields.Selection([('1', 'Ⅰ'), ('2', 'Ⅱ'), ('3', 'Ⅲ')], string='管理类别', default='1')
 
     udi_flbm = fields.Many2one('medical.device.category', related='udi_data_id.flbm', store=True)
 
