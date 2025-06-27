c7896b92a16a52b4ab75efb471116e40c6d5c4c4
He Jian <hej@eabax.com>
2025-06-27 16:26:48 +0800

    [IMP] udi_data: 后台导入“医疗器械分类”，减少模块安装和升级时间.

--- udi_data/__init__.py
+++ udi_data/__init__.py
@@ -6,21 +6,21 @@ def _udi_data_import_data(env):
     from odoo.modules import get_module_path
 
     # 导入医疗器械分类
-    # file_path = get_module_path('udi_data') + '/data/medical.device.category.csv'
-    # with open(file_path, 'r', encoding='utf-8') as file:
-    #     file_content = file.read()
+    file_path = get_module_path('udi_data') + '/data/medical.device.category.csv'
+    with open(file_path, 'r', encoding='utf-8') as file:
+        file_content = file.read()
 
-    #     import_obj = env['base_import.import'].create({
-    #         'res_model': 'medical.device.category',
-    #         'file': file_content,
-    #         'file_name': 'medical.device.category.csv',
-    #         'file_type': 'text/csv',
-    #     })
-    #     import_obj.execute_import(
-    #         ['id', 'name', 'code', 'parent_id/id', 'cpms', 'yqyt', 'pmjl', 'gllb'],
-    #         ['id', 'name', 'code', 'parent_id/id', 'cpms', 'yqyt', 'pmjl', 'gllb'],
-    #         {
-    #             'has_headers': True, 'encoding': 'utf-8', 'separator': ',', 'quoting': '"',
-    #             'use_queue': True, 'chunk_size': 400, 'priority': 100,
-    #         }
-    #     )
+        import_obj = env['base_import.import'].create({
+            'res_model': 'medical.device.category',
+            'file': file_content,
+            'file_name': 'medical.device.category.csv',
+            'file_type': 'text/csv',
+        })
+        import_obj.execute_import(
+            ['id', 'name', 'code', 'parent_id/id', 'cpms', 'yqyt', 'pmjl', 'gllb'],
+            ['id', 'name', 'code', 'parent_id/id', 'cpms', 'yqyt', 'pmjl', 'gllb'],
+            {
+                'has_headers': True, 'encoding': 'utf-8', 'separator': ',', 'quoting': '"',
+                'use_queue': True, 'chunk_size': 400, 'priority': 100,
+            }
+        )
