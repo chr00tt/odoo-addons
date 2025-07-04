d0ebe9da9bfac91e35ff2c4c27cd00fa89fc14e6
He Jian <hej@eabax.com>
2025-07-04 17:22:42 +0800

    [IMP] medical_consumables_stock: 入库明细列表补充字段:注册证号、标签、生产厂家、产品类别.

--- medical_consumables_stock/models/stock_move.py
+++ medical_consumables_stock/models/stock_move.py
@@ -6,3 +6,14 @@ class StockMove(models.Model):
     _inherit = "stock.move"
 
     product_ggxh = fields.Char(related="product_id.ggxh")
+    registration_number = fields.Char('注册证号', related="product_id.registration_number")
+    product_tag_ids = fields.Many2many(
+        'product.tag', string='产品标签',
+        related='product_id.product_tag_ids')
+    manufacturer_id = fields.Many2one(
+        'res.partner', string='生产厂家',
+        related='product_id.manufacturer_id')
+    categ_id = fields.Many2one(
+        'product.category', string='产品类别',
+        related='product_id.categ_id')
+
