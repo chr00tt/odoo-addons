f57e05a4c8ede992921427c1d75fcdcbf38bac51
He Jian <hej@eabax.com>
2025-07-22 14:14:24 +0800

    [IMP] medical_consumables_product: 选择产品对话框：列表应包含注册证号、生产厂家等信息。

--- medical_consumables_product/__manifest__.py
+++ medical_consumables_product/__manifest__.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
     'name': '医用耗材 - 产品和价格表',
     'author': 'He Jian',
     'website': 'https://github.com/chr00tt/odoo-addons',
     'category': 'Inventory/Inventory',
-    'depends': ['product'],
+    'depends': [
+        'product',
+
+        'product_manufacturer',
+    ],
     'data': [
         'views/product_supplierinfo_views.xml',
         'views/product_template_views.xml',
