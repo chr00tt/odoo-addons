5dc542b76372e9c1ff89c9e7020d093c7378ad2f
He Jian <hej@eabax.com>
2025-08-06 14:44:41 +0800

    [ADD] l10n_cn_hospital_stock_account: 添加医院库存物品类别.

---
+++ l10n_cn_hospital_stock_account/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': '中国 - 医院仓库管理会计',
+    'author': "He Jian",
+    'website': 'https://github.com/chr00tt/odoo-addons',
+    'depends': ['l10n_cn_hospital', 'stock_account'],
+    'category': 'Hidden',
+    'data': [
+        'data/product_data.xml',
+    ],
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
