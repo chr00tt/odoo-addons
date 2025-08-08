7eae776333f8dd87ee97a1b61782b11bd9d1100f
He Jian <hej@eabax.com>
2025-08-08 17:15:31 +0800

    [IMP] medical_consumables_product: 供应目录需要添加注册证号、生产厂家、医保编码等信息.

--- medical_consumables_product/models/product_supplierinfo.py
+++ medical_consumables_product/models/product_supplierinfo.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import api, fields, models, _
 
@@ -7,3 +6,6 @@ class SupplierInfo(models.Model):
     _inherit = "product.supplierinfo"
 
     product_ggxh = fields.Char('规格/型号', related="product_tmpl_id.ggxh")
+    registration_number = fields.Char('注册证号', related="product_tmpl_id.registration_number")
+    manufacturer_id = fields.Many2one('res.partner', '生产厂家', related="product_tmpl_id.manufacturer_id")
+    ybbm = fields.Char('医保编码', related="product_tmpl_id.ybbm")
