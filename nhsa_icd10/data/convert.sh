cd8f2addb77d5d618b14ca9aa3933b1055e5d6c6
He Jian <hej@eabax.com>
2025-09-17 17:02:09 +0800

    fix

--- nhsa_icd10/data/convert.sh
+++ nhsa_icd10/data/convert.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 echo 'id,name,code,parent_id/id' > icd10.category.csv
-awk -v FPAT='([^,]+)|("[^"]+")' 'NR>1 {
+awk -v FPAT='([^,]*)|("[^"]*")' 'NR>1 {
 print "icd10_category_chapter_" $2 "," $3 "," $2 ",icd10_category_all"
 }' output.csv | uniq >> icd10.category.csv
-awk -v FPAT='([^,]+)|("[^"]+")' 'NR>1 {
+awk -v FPAT='([^,]*)|("[^"]*")' 'NR>1 {
 print "icd10_category_block_" $4 "," $5 "," $4 ",icd10_category_chapter_" $2
 }' output.csv | uniq >> icd10.category.csv
-awk -v FPAT='([^,]+)|("[^"]+")' 'NR>1 && $6!="" {
+awk -v FPAT='([^,]*)|("[^"]*")' 'NR>1 && $6!="" {
 print "icd10_category_category_" $6 "," $7 "," $6 ",icd10_category_block_" $4
 }' output.csv | uniq >> icd10.category.csv
-awk -v FPAT='([^,]+)|("[^"]+")' 'NR>1 && $8!="" {
+awk -v FPAT='([^,]*)|("[^"]*")' 'NR>1 && $8!="" {
 print "icd10_category_subcategory_" $8 "," $9 "," $8 ",icd10_category_category_" $6
 }' output.csv | uniq >> icd10.category.csv
 
 echo 'id,code,name,category_id/id' > icd10.code.csv
-awk -v FPAT='([^,]+)|("[^"]+")' 'NR>1 && $10!="" {
+awk -v FPAT='([^,]*)|("[^"]*")' 'NR>1 && $10!="" {
 if ($8 != "") {
     category = "icd10_category_subcategory_" $8
 } else if ($6 != "") {
