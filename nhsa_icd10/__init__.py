a71288fcc7947241df01dc8bb3ac0442907ad464
He Jian <hej@eabax.com>
2025-09-17 15:31:57 +0800

    fix

--- nhsa_icd10/__init__.py
+++ nhsa_icd10/__init__.py
@@ -6,24 +6,24 @@ def _import_nhsa_hc(env):
     from odoo.modules import get_module_path
 
     # 导入分类
-    file_path = get_module_path('nhsa_icd10') + '/data/icd10.category.csv'
-    with open(file_path, 'r', encoding='utf-8') as file:
-        file_content = file.read()
+    # file_path = get_module_path('nhsa_icd10') + '/data/icd10.category.csv'
+    # with open(file_path, 'r', encoding='utf-8') as file:
+    #     file_content = file.read()
 
-        import_obj = env['base_import.import'].create({
-            'res_model': 'icd10.category',
-            'file': file_content,
-            'file_name': 'icd10.category.csv',
-            'file_type': 'text/csv',
-        })
-        import_obj.execute_import(
-            ['id', 'name', 'code', 'parent_id/id'],
-            ['id', 'name', 'code', 'parent_id/id'],
-            {
-                'has_headers': True, 'encoding': 'utf-8', 'separator': ',', 'quoting': '"',
-                'use_queue': True, 'chunk_size': 1000, 'priority': 100,
-            }
-        )
+    #     import_obj = env['base_import.import'].create({
+    #         'res_model': 'icd10.category',
+    #         'file': file_content,
+    #         'file_name': 'icd10.category.csv',
+    #         'file_type': 'text/csv',
+    #     })
+    #     import_obj.execute_import(
+    #         ['id', 'name', 'code', 'parent_id/id'],
+    #         ['id', 'name', 'code', 'parent_id/id'],
+    #         {
+    #             'has_headers': True, 'encoding': 'utf-8', 'separator': ',', 'quoting': '"',
+    #             'use_queue': True, 'chunk_size': 1000, 'priority': 100,
+    #         }
+    #     )
 
     # 导入诊断代码
     file_path = get_module_path('nhsa_icd10') + '/data/icd10.code.csv'
--- nhsa_icd10/data/convert.sh
+++ nhsa_icd10/data/convert.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
 
 echo 'id,name,code,parent_id/id' > icd10.category.csv
-awk -F ',' 'NR>1 {print "icd10_category_chapter_" $2 "," $3 "," $2 ",icd10_category_all"}' output.csv | uniq >> icd10.category.csv
-awk -F ',' 'NR>1 {print "icd10_category_block_" $4 "," $5 "," $4 ",icd10_category_chapter_" $2}' output.csv | uniq >> icd10.category.csv
-awk -F ',' 'NR>1 {print "icd10_category_category_" $6 "," $7 "," $6 ",icd10_category_block_" $4}' output.csv | uniq >> icd10.category.csv
-awk -F ',' 'NR>1 {print "icd10_category_subcategory_" $8 "," $9 "," $8 ",icd10_category_category_" $6}' output.csv | uniq >> icd10.category.csv
+awk -v FPAT='([^,]+)|("[^"]+")' 'NR>1 {print "icd10_category_chapter_" $2 "," $3 "," $2 ",icd10_category_all"}' output.csv | uniq >> icd10.category.csv
+awk -v FPAT='([^,]+)|("[^"]+")' 'NR>1 {print "icd10_category_block_" $4 "," $5 "," $4 ",icd10_category_chapter_" $2}' output.csv | uniq >> icd10.category.csv
+awk -v FPAT='([^,]+)|("[^"]+")' 'NR>1 && $6!="" {print "icd10_category_category_" $6 "," $7 "," $6 ",icd10_category_block_" $4}' output.csv | uniq >> icd10.category.csv
+awk -v FPAT='([^,]+)|("[^"]+")' 'NR>1 && $8!="" {print "icd10_category_subcategory_" $8 "," $9 "," $8 ",icd10_category_category_" $6}' output.csv | uniq >> icd10.category.csv
+
+echo 'id,code,name,category_id/id' > icd10.code.csv
+awk '
+BEGIN {
+    # FPAT定义字段模式：非逗号字符 或 被引号包围的字符
+    FPAT="([^,]*)|(\"[^\"]+\")"
+    OFS = ","
+}
+NR > 1 && $10!="" {
+    # 检查是否有足够的列且关键字段不为空
+        code = $10
+        name = $11
+        category = $8
+
+        # 构造输出行
+        id_field = "icd10_diagnostic_code_" code
+        category_field = "icd10_category_subcategory_" category
+
+        # 输出行
+        print id_field, code, name, category_field
+}
+' output.csv | uniq >> icd10.code.csv
