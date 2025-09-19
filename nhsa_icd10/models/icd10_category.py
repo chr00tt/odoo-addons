0b392534e9040ae0db99aeeb1ac834af15526500
Liyh <380568933@qq.com>
2025-09-19 14:46:38 +0800

    ICD-10 分类修改为完全名称

--- nhsa_icd10/models/icd10_category.py
+++ nhsa_icd10/models/icd10_category.py
@@ -5,7 +5,7 @@ from odoo import models, api, fields
 class Icd10Category(models.Model):
     _name = 'icd10.category'
     _description = 'ICD-10 分类'
-    _rec_name = 'name'
+    _rec_name = 'complete_name'
 
     name = fields.Char(string='名称', required=True)
     code = fields.Char(string='分类代码', required=True, index=True)
@@ -15,6 +15,9 @@ class Icd10Category(models.Model):
     code_count = fields.Integer(
         '# 代码', compute='_compute_code_count',
         help="该分类下的ICD-10代码数量（不包括子分类）")
+    complete_name = fields.Char(
+        '完整名称', compute='_compute_complete_name', recursive=True,
+        store=True)
 
     def _compute_code_count(self):
         read_group_res = self.env['icd10.code']._read_group([('category_id', 'child_of', self.ids)], ['category_id'], ['__count'])
@@ -24,3 +27,18 @@ class Icd10Category(models.Model):
             for sub_categ_id in categ.search([('id', 'child_of', categ.ids)]).ids:
                 code_count += group_data.get(sub_categ_id, 0)
             categ.code_count = code_count
+
+    @api.depends('name', 'parent_id.complete_name')
+    def _compute_complete_name(self):
+        for category in self:
+            if category.parent_id:
+                category.complete_name = '%s / %s' % (category.parent_id.complete_name, category.name)
+            else:
+                category.complete_name = category.name
+
+    @api.depends_context('hierarchical_naming')
+    def _compute_display_name(self):
+        if self.env.context.get('hierarchical_naming', True):
+            return super()._compute_display_name()
+        for record in self:
+            record.display_name = record.name
