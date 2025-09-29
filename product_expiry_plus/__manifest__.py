6fd41dfaecc3a714db2af263a977d1cfdb8d5ce6
He Jian <hej@eabax.com>
2025-09-29 10:22:24 +0800

    [ADD] product_expiry_plus.

---
+++ product_expiry_plus/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Products Expiration Date Plus',
+    'author': 'He Jian',
+    'website': 'https://github.com/chr00tt/odoo-addons',
+    'category': 'Inventory/Inventory',
+    'depends': ['product_expiry'],
+    'data': [
+        'views/stock_move_line_views.xml',
+    ],
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
