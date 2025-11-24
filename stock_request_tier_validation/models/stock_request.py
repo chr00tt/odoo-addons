a94062f78995d5c1ef0f554e56f7bcca150ebafd
wait <634591639@qq.com>
2025-11-24 19:43:00 +0800

    [FIX]

--- stock_request_tier_validation/models/stock_request.py
+++ stock_request_tier_validation/models/stock_request.py
@@ -1,6 +1,8 @@
 # -*- encoding: utf-8 -*-
 
 from odoo import _, api, fields, models
+from odoo.exceptions import UserError
+
 
 class StockRequest(models.Model):
     _name = "stock.request"
@@ -14,8 +16,17 @@ class StockRequest(models.Model):
         vals = {"state": "open"}
 
         # 检查是否需要审批
-        self._tier_validation_check_state_on_write(vals)
+        # self._tier_validation_check_state_on_write(vals)
         self._tier_validation_check_write_allowed(vals)
         self._tier_validation_check_write_remove_reviews(vals)
 
         return super().action_confirm()
+    
+    def validate_tier(self):
+        names = []
+        for res in self:
+            if not res.review_ids:
+                names.append(res.name)
+        if names:
+            raise UserError(_("以下订单未提交审批 %s") % ', '.join(names))
+        return super().validate_tier()
